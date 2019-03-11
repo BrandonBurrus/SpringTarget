c05f2a59ca9df05a9b1c9d2f55cdb956b20aafd2
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-03-10 18:19:30 -0700

    Adding all existing project files

--- Spring Target/ViewController.swift
+++ Spring Target/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var spring: CGFloat = 0 {
+        didSet {
+            springLabel.text = "\(floor(spring * 100) / 100)"
+        }
+    }
+    var damping: CGFloat = 0 {
+        didSet {
+            dampingLabel.text = "\(floor(damping * 100) / 100)"
+        }
     }
 
+    @IBOutlet weak var springLabel: UILabel!
+    @IBOutlet weak var dampingLabel: UILabel!
+    @IBOutlet weak var ballView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ballView.center = view.center
+        spring = 7
+        damping = 0.5
+    }
 
+    @IBAction func tap(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            UIView.animate(
+                withDuration: 0.5,
+                delay: 0,
+                usingSpringWithDamping: damping,
+                initialSpringVelocity: spring,
+                options: [],
+                animations: {
+                    self.ballView.center = sender.location(in: self.view)
+                }
+            )
+            
+        }
+    }
+    
+    @IBAction func pan(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        let change = translation.y * -0.0033
+        switch sender.view {
+        case springLabel:
+            spring += change
+        case dampingLabel:
+            damping += change
+        default:
+            fatalError("Unhandled panning object value not associated with a view")
+        }
+        sender.setTranslation(CGPoint(x: translation.x, y: 0), in: view)
+    }
 }
-
